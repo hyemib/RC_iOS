63f8aa17bbc5140f81a1c3d0c4669afcdcd449fa
Hhyemm <hyem0127@gmail.com>
2022-04-27 01:36:02 +0900

    Add: first commit

---
+++ catchtable/catchtable/BannerCell.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var bannerTitle1View: UIView!
+    @IBOutlet weak var bannerTitle1: UILabel!
+    @IBOutlet weak var bannerTitle2: UILabel!
+    @IBOutlet weak var bannerTitle3: UILabel!
+}
