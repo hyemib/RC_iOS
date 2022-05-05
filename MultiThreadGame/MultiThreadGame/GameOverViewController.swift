57bc8e033499e7f2767be8e9e62347b5bc781c23
Hhyemm <hyem0127@gmail.com>
2022-05-05 21:54:33 +0900

    Add : 기능 추가

---
+++ MultiThreadGame/MultiThreadGame/GameOverViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class GameOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- MultiThreadGame/MultiThreadGame/MainViewController.swift
+++ MultiThreadGame/MultiThreadGame/MainViewController.swift
@@ -3,21 +3,30 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var heart: UIImageView!
     @IBOutlet weak var ryan: UIImageView!
+    
+    var timer: Timer?
     var positionX: CGFloat!
     var ryanY: CGFloat! = 700
+    var ryanPosition: CGFloat! = 215
     var tubes = [UIImageView]()
     var snows = [UIImageView]()
+    var heartCount = 3
+    var tubeIndex = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        heart.image = UIImage(named: "heart3")
         
         self.ryan.frame = CGRect(x: ryan.center.x, y: ryanY, width: 90, height: 110)
         
         DispatchQueue.global().async {
             let isRunning = true
             let runLoop = RunLoop.current
-            Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+            self.timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
                 DispatchQueue.main.sync {
                     self.createTube()
                 }
@@ -39,18 +48,28 @@ class MainViewController: UIViewController {
         DispatchQueue.main.async {
             UIView.animate(withDuration: 0.3) {
                 self.ryan.frame = CGRect(x: touch.x, y: self.ryanY, width: 90, height: 110)
+                self.ryanPosition = touch.x
+
             }
         }
-        let snow = UIImageView(image: UIImage(named: "snow"))
-        snow.contentMode = .scaleAspectFit
-        snow.frame = CGRect(x: touch.x, y: ryanY, width: 40, height: 40)
-        snows.append(snow)
-        DispatchQueue.main.async {
-            UIView.animate(withDuration: 0.3, animations: {
-                snow.frame = CGRect(x: touch.x, y: touch.y, width: 40, height: 40)
-            })
+        if touch.y <= 700 {
+            let snow = UIImageView(image: UIImage(named: "snow"))
+            snow.contentMode = .scaleAspectFit
+            snow.frame = CGRect(x: touch.x, y: ryanY, width: 40, height: 40)
+            snows.append(snow)
+            DispatchQueue.main.async {
+                UIView.animate(withDuration: 0.3, animations: {
+                    snow.frame = CGRect(x: touch.x, y: touch.y, width: 40, height: 40)
+                })
+            }
+            self.view.addSubview(snow)
+            for i in 0..<tubes.count {
+                print(tubes[i].frame.minX, tubes[i].frame.maxX)
+                if tubes[i].frame.minX <= touch.x && tubes[i].frame.maxX >= touch.x {
+                    tubes[i].removeFromSuperview()
+                }
+            }
         }
-        self.view.addSubview(snow)
     }
 
     
@@ -69,56 +88,79 @@ class MainViewController: UIViewController {
         let y = self.view.frame.height/2+30
         let rangeX =  CGFloat(Int.random(in: 10...Int(x)))
         let rangeY = y/2+CGFloat(Int.random(in: 0...50))
+        let tubeWidth = 60
+        let tubeHeiht = 80
         tube.frame = CGRect(x: rangeX, y: 210, width: 60, height: 80)
         tubes.append(tube)
+        tubeIndex += 1
+        
+        moveTube(tube: tube, rangeX: rangeX, rangeY: rangeY, moveY: 0, width: CGFloat(tubeWidth+8), heiht: CGFloat(tubeHeiht+8))
+        
+        self.view.addSubview(tube)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 7) {
+            self.moveTube(tube: tube, rangeX: rangeX, rangeY: rangeY, moveY: 90, width: CGFloat(tubeWidth+15), heiht: CGFloat(tubeHeiht+15))
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 7) {
+                self.moveTube(tube: tube, rangeX: rangeX, rangeY: rangeY, moveY: 220, width: CGFloat(tubeWidth+25), heiht: CGFloat(tubeHeiht+25))
+            }
+        }
+        
+    }
+    
+    func moveTube(tube: UIImageView, rangeX: CGFloat, rangeY: CGFloat, moveY: CGFloat, width: CGFloat, heiht: CGFloat) {
+        let tubePosition = CGFloat(Int.random(in: Int(rangeX-60)...Int(rangeX+60)))
+        
         UIView.animate(withDuration: 3, animations: {
-            tube.frame = CGRect(x: rangeX, y: rangeY+10, width: 68, height: 88)
-            
+            tube.frame = CGRect(x: tubePosition, y: rangeY+moveY, width: width, height: heiht)
         }) { _ in
-            tube.image = UIImage(named: "tube2")
             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
                 tube.image = UIImage(named: "tube3")
                 let snow = UIImageView(image: UIImage(named: "snow"))
                 snow.contentMode = .scaleAspectFit
-                snow.frame = CGRect(x: rangeX, y: rangeY+50, width: 40, height: 40)
+                snow.frame = CGRect(x: tubePosition, y: rangeY+moveY+50, width: 40, height: 40)
                 DispatchQueue.main.async {
                     UIView.animate(withDuration: 0.6, animations: {
-                        snow.frame = CGRect(x: rangeX, y: self.view.frame.height, width: 40, height: 40)
+                        snow.frame = CGRect(x: tubePosition, y: self.view.frame.height, width: 40, height: 40)
                     })
                     self.view.addSubview(snow)
                 }
+                
+                if self.ryanPosition >= rangeX-30 && self.ryanPosition <= rangeX+30 {
+                    self.ryan.image = UIImage(named: "ryan2")
+                    self.changeHeart()
+                }
+                
                 DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
                     tube.image = UIImage(named: "tube1")
                     snow.removeFromSuperview()
+                    self.ryan.image = UIImage(named: "ryan1")
                 }
             }
         }
-        self.view.addSubview(tube)
-        
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 7) {
-            UIView.animate(withDuration: 3, animations: {
-                
-                tube.frame = CGRect(x: rangeX, y: rangeY+70, width: 70, height: 90)
-                
-            }) { _ in
-                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
-                    tube.image = UIImage(named: "tube3")
-                    let snow = UIImageView(image: UIImage(named: "snow"))
-                    snow.contentMode = .scaleAspectFit
-                    snow.frame = CGRect(x: rangeX, y: rangeY+120, width: 40, height: 40)
-                    DispatchQueue.main.async {
-                        UIView.animate(withDuration: 0.6, animations: {
-                            snow.frame = CGRect(x: rangeX, y: self.view.frame.height, width: 40, height: 40)
-                        })
-                        self.view.addSubview(snow)
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
-                        tube.image = UIImage(named: "tube1")
-                        snow.removeFromSuperview()
-                    }
-                }
+    }
+    
+    func changeHeart() {
+        switch heartCount {
+        case 3 :
+            heart.image = UIImage(named: "heart2")
+            heartCount -= 1
+        case 2 :
+            heart.image = UIImage(named: "heart1")
+            heartCount -= 1
+        default:
+            heart.image = UIImage(named: "heart0")
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                self.timer?.invalidate()
+                self.gameOver()
             }
         }
+    }
+    
+    func gameOver() {
+        /*
+        guard let pauseVC = self.storyboard?.instantiateViewController(withIdentifier: "GameOverViewController") as? GameOverViewController else { return }
+        pauseVC.modalPresentationStyle = .fullScreen
+        self.present(pauseVC, animated: false, completion: nil)*/
     }
 }
 
